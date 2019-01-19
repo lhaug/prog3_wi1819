ecc92d51673cc0711c1c2d7c505428fa5692529b
lhaug <lea.j.i.haugrund@student.hs-rm.de>
2019-01-19 14:07:08 +0100

    a39 almost finished, some questions

--- Python/python2019/a39/a39.py
+++ Python/python2019/a39/a39.py
@@ -3,6 +3,7 @@ Created on 15.01.2019
 
 @author: Lea
 '''
+
 class Messwert():
    
     def __init__(self, *line):
@@ -19,10 +20,39 @@ class Messwert():
         
     def __str__(self):
         return("'{}',{}".format(str(self._zeitpunkt), str(self._temperatur)))
+    
+    def __repr__(self):
+        return("'{}',{}".format(str(self._zeitpunkt), str(self._temperatur)))
+    
+    #Problem: other ist ein Tupel, cast in Messwert geht auch nicht
+    def __eq__(self, other):
+        x = self._zeitpunkt
+        y = other._zeitpunkt
+        if x == y:
+            if self._temperatur == other._temperatur:
+                return True
+        else:
+            return False
         
-
+    def __lt__(self, other):
+        if self._zeitpunkt < other._zeitpunkt:
+            return True
+        elif self._zeitpunkt == self._zeitpunkt and self._temperatur < other._temperatur:
+            return True
+        else:
+            return False
+    
 if __name__ == "__main__":
-    mw = Messwert("'2019-01-11 17:45:01.356640',19.5")
-    print(mw)
-    mw = Messwert('2019-01-11 17:45:01.356640',19.5)   
-    print(mw)     
+    mw1 = Messwert("'2019-01-15 17:45:01.356640',19.5")
+    print(mw1)
+    mw2 = Messwert('2019-01-11 16:45:01.356640',19.5)   
+    print(mw2) 
+    mw3 = Messwert("'2019-01-15 17:45:01.356640',20.5")
+    print(mw3)
+    mwlist = [mw1, mw2, mw3]
+    #print(eval(repr(mw))== mw)
+    print(mw1 < mw2)
+    print(mw2 < mw1)
+    print(mw1 < mw3)
+    print(mw3 < mw1)
+    print(sorted(mwlist))
