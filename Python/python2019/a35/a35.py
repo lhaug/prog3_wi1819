118a24807f8c85f693526c2fa797089163516556
lhaug <lea.j.i.haugrund@student.hs-rm.de>
2019-01-19 12:43:34 +0100

    a34 und a35 fertig

--- Python/python2019/a35/a35.py
+++ Python/python2019/a35/a35.py
@@ -5,28 +5,28 @@ Created on 14.01.2019
 '''
 # coding: utf-8
 from sys import argv
-from itertools import count
 
 dat = argv[-1]
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet = "eniastruhdlcmogkwbzfvpjxyq"
 
 all_chars = [char.lower() for line in open(dat) for char in line]
 
 #Buchstaben auszaehlen
 counted = {}
-print(counted.get('a'))
 for x in all_chars:
     if counted.get(x):
         counted[x]=str(int(counted[x])+1)
     elif x in alphabet:
         counted[x] = str(1)
 
-print(counted)
 
 #counted sortieren und umrechentabelle erstellen
-table = dict(zip(sorted(counted, key=counted.get, reverse=True),alphabet))
-x = sorted(counted.items(), key=lambda x : int(x[1]))
-x = sorted((key) for (key,value) in counted.items())
+sorted_charcounter_array = sorted(counted.items(), key=lambda x : int(x[1]), reverse=True)
+sorted_chars = [x[0] for x in sorted_charcounter_array]
+table = dict(zip(sorted_chars,alphabet))
+
+x = "".join(map(lambda x: table.get(x, x), all_chars))
+
 print(x)
         
