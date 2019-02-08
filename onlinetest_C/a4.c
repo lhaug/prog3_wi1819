b8d07152fb9064ed78352f18c5ef033ae2eb02cc
lhaug <33481748+lhaug@users.noreply.github.com>
2019-02-08 17:50:21 +0100

    c onlinetest a4.c done

---
+++ onlinetest_C/a4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+void schwabify(char *input, char output[]){
+  char* akt = input;
+  char* test;
+  char* out = output;
+  while(*akt!='\0'){
+    /*verdacht pruefen*/
+    test = akt;
+    if(*akt == '.'){
+      strcpy(out, ", woisch?");
+      out = out+9;
+      akt++;
+    }else if( (*akt=='h')&&(*(test+1)=='a')&&(*(test+2)=='l')&&(*(test+3)=='t') ){
+      strcpy(out, "heb");
+      out = out+3;
+      akt = akt+4;
+    }else if( (*akt == 'i')&&(*(test+1)=='s')&&(*(test+2)=='t') ){
+      strcpy(out, "isch");
+      out = out+4;
+      akt = akt+3;
+    }else{
+      /*kein ersetzen*/
+      *out = *akt;
+      out++;
+      akt++;
+    }
+  }
+  *out = '\0'; 
+}
